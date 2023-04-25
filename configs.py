4df4aa11df26f48801c16aa63372feb8e70c6ea3
Pradarsh12 <117892231+Pradarsh12@users.noreply.github.com>
2023-04-25 16:58:33 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -30,20 +30,20 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @soonbotmaker0
 
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
+👥 **Contact Admin:** [Contact](https://t.me/B_PLAB)
 
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+📢 **Paid Bot Maker:** [Pradarsh](https://t.me/soonbotmaker0)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @soonbotmaker0
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
+[Contact](https://t.em/B_PLAB) (owner )
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
